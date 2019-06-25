b361fdfff7196d5cb87ad19b8defb1c224b8e564
Lam T Duong <lduong6@masonlive.gmu.edu>
2019-06-25 18:57:50 -0400

    Added testing folder to .gitignore

--- Testing/MatrixTests.cpp
+++ Testing/MatrixTests.cpp
@@ -1,16 +1,9 @@
-#include "../Matrix.h"
-#include "../Matrix.cpp"
-#include <string>
+#define CATCH_CONFIG_MAIN
 #include "catch2.hpp"
+#include "../Matrix.h"
 
-TEST_CASE("EmptyMatrixConstructioNTests")
-{
+TEST_CASE("Just an empty 1x1 matrix", "[single-file]") {
 	Matrix matrix1(1, 1);
 	matrix1.populateMatrix();
 	REQUIRE(matrix1.toString() == "0");
 }
-
-
-// Compile & run:
-// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 010-TestCase 010-TestCase.cpp && 010-TestCase --success
-// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 010-TestCase.cpp && 010-TestCase --success
