495b6c842146c69a19e894e98f24570c0b9281c8
Lam Duong <lamthachduong@gmail.com>
2019-07-05 13:51:36 -0400

    Started catch2 test environment

--- MatrixTests.cpp
+++ MatrixTests.cpp
@@ -3,7 +3,12 @@
 
 TEST_CASE("Constructors are tested", "[Matrix]")
 {
-    Matrix matrix1(1,1);
+    Matrix matrix1(1, 1);
+	Matrix matrix2(2, 1);
+	Matrix matrix3(1, 2);
+	Matrix matrix4(3, 3);
+	Matrix matrix5(12, 20);
+	Matrix matrix6;
     REQUIRE(matrix1.toString() == "0\n");
 }
 
